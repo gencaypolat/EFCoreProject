b162796748afcf81af098e6e94601b46a2efc335
gencaypolat <gencaypolat@gmail.com>
2024-03-21 18:39:42 +0300

    initial commit

---
+++ Entities/PersonsDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class PersonsDbContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set;}
+        public DbSet<Country> Countries {  get; set;}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Country>().ToTable("Countries");
+            modelBuilder.Entity<Person>().ToTable("Persons");
+        }
+    }
+}
