8796e0e979abc193504ad1478c91d98fb7539161
gencaypolat <gencaypolat@gmail.com>
2024-03-26 22:26:25 +0300

    TIN column added to Persons table

---
+++ Entities/Migrations/20240326175024_TINColumnAddedPersons.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Entities.Migrations
+{
+    public partial class TINColumnAddedPersons : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TIN",
+                table: "Persons",
+                type: "nvarchar(max)",
+                nullable: true,
+                defaultValue: "defaultTIN");
+
+            // Adding the default value to the current datas
+            migrationBuilder.Sql("UPDATE Persons SET TIN = 'defaultTIN' WHERE TIN IS NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TIN",
+                table: "Persons");
+        }
+    }
+}
--- Entities/Person.cs
+++ Entities/Person.cs
@@ -3,23 +3,24 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Entities
 {
-  /// <summary>
-  /// Person domain model class
-  /// </summary>
-  public class Person
-  {
-    [Key]
-    public Guid PersonID { get; set; }
-    [StringLength(40)]
-    public string? PersonName { get; set; }
-    [StringLength(40)]
-    public string? Email { get; set; }
-    public DateTime? DateOfBirth { get; set; }
-    [StringLength(10)]
-    public string? Gender { get; set; }
-    public Guid? CountryID { get; set; }
-    public string? Address { get; set; }
-    //bit 
-    public bool ReceiveNewsLetters { get; set; }
-  }
+    /// <summary>
+    /// Person domain model class
+    /// </summary>
+    public class Person
+    {
+        [Key]
+        public Guid PersonID { get; set; }
+        [StringLength(40)]
+        public string? PersonName { get; set; }
+        [StringLength(40)]
+        public string? Email { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        [StringLength(10)]
+        public string? Gender { get; set; }
+        public Guid? CountryID { get; set; }
+        public string? Address { get; set; }
+        //bit 
+        public bool ReceiveNewsLetters { get; set; }
+        public string? TIN { get; set; }
+    }
 }
